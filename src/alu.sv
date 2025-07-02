d9cfa6275cc04ec7ed0fb9662d2aa935746c761a
granitespinoza <112722319+granitespinoza@users.noreply.github.com>
2025-07-02 13:35:44 -0500

    Fix SUB flag computation

--- src/alu.sv
+++ src/alu.sv
@@ -6,6 +6,7 @@ module alu(
     output reg [3:0] ALUFlags
 );
     wire [32:0] sum = {a[31], a} + {b[31], b};
+    wire [32:0] diff = {a[31], a} + {~b[31], ~b} + 1'b1;
 
     always @(*)
         case (ALUControl)
@@ -20,7 +21,19 @@ module alu(
     always @(*) begin
         ALUFlags[3] = Result[31];
         ALUFlags[2] = (Result == 32'h00000000);
-        ALUFlags[1] = (ALUControl==3'b000 || ALUControl==3'b001) ? sum[32] : 1'b0;
-        ALUFlags[0] = (ALUControl==3'b000 || ALUControl==3'b001) ? (sum[31]^a[31]^b[31]^sum[32]) : 1'b0;
+        case (ALUControl)
+            3'b000: begin
+                ALUFlags[1] = sum[32];
+                ALUFlags[0] = sum[31] ^ a[31] ^ b[31] ^ sum[32];
+            end
+            3'b001: begin
+                ALUFlags[1] = diff[32];
+                ALUFlags[0] = diff[31] ^ a[31] ^ ~b[31] ^ diff[32];
+            end
+            default: begin
+                ALUFlags[1] = 1'b0;
+                ALUFlags[0] = 1'b0;
+            end
+        endcase
     end
 endmodule
